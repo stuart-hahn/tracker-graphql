11cabcfd5a5229c4bba335246759223e8bc51f6c
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-08-26 17:20:43 -0400

    extract prisma secret to env variables

--- src/prisma.js
+++ src/prisma.js
@@ -3,7 +3,7 @@ import { Prisma } from 'prisma-binding'
 const prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
   endpoint: process.env.PRISMA_ENDPOINT,
-  secret: 'thisisthesecret'
+  secret: process.env.PRISMA_SECRET
 })
 
 export default prisma
